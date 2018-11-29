423826e475f760d2aa8b644fb010a057747536fd
WaynePL <wayneplivingston@gmail.com>
2018-11-29 16:14:37 -0500

    Adds recipes to the meal and can start cooking the recipe

--- src/planning-view/Planning.js
+++ src/planning-view/Planning.js
@@ -39,6 +39,17 @@ export class Planning extends React.Component {
         }))
     }
 
+    fetchAddRecipeToMeal(recipeToAdd){
+        fetch(`http://localhost:8080/api/meals/addRecipe`, {
+            method: "POST",
+            body : JSON.stringify({
+                mealId : this.state.newMeal.id,
+                recipeId : recipeToAdd
+            })
+        }).then(res => res.json()).then(data => this.setState({newMeal : data}))
+        console.log(recipeToAdd)
+    }
+
     render(){
         let arrayOfMeals = []
         let arrayOfRecipes = []
@@ -73,12 +84,19 @@ export class Planning extends React.Component {
                     Recipes go here!
                     <ul>
                         {arrayOfRecipes.map(recipe => {
-                            return <li key={recipe.id} >{recipe.name}</li>                
+                            return <li key={recipe.id} onClick={() => {this.fetchAddRecipeToMeal(recipe.id)}}>{recipe.name}</li>                
                         })}
                     </ul>
 
                     <div>
-                        {this.state.newMeal.name}
+                        {this.state.newMeal.name} {this.state.newMeal.length} Seconds
+                        <ul>
+                            {this.state.newMeal.recipes.map(recipe => {
+                                return <li key={recipe.id} >{recipe.name}<span>[X]</span></li>                
+                            })
+                            }
+                        </ul>
+                        <button onClick={() => {this.props.setMealId(this.state.newMeal.id)}}>Cook</button>
                     </div>
                 </section>
             )
