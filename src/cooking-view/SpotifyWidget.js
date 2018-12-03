aee605403808aab142ea5be8e3c660bbeb793051
maxmraz <max.mraz.11@gmail.com>
2018-12-03 12:43:14 -0500

    add actual spotify widget to cooking view

--- src/cooking-view/SpotifyWidget.js
+++ src/cooking-view/SpotifyWidget.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import './css/spotifyWidget.css'
 
+
+
 class spotifyWidget extends React.Component {
+
+    // <span className="album-art">&#9633;</span>
+    // <div className="middle-container">
+    //     <h4 className="song-title">Ain't No Mountain High Enough</h4>
+    //     <span className="playbar-background">.</span>
+    //     <span className="playbar-foreground">.</span>
+    // </div>
+    // <button className="pause-button"><h2>||</h2></button>
+    // <button className="menu">&#9776;</button>
 
     render(){
 
         return(
             <div className="spotify-widget">
-                <span className="album-art">&#9633;</span>
-                <div className="middle-container">
-                    <h4 className="song-title">Ain't No Mountain High Enough</h4>
-                    <span className="playbar-background">.</span>
-                    <span className="playbar-foreground">.</span>
-                </div>
-                <button className="pause-button"><h2>||</h2></button>
-                <button className="menu">&#9776;</button>
+            <iframe src="https://open.spotify.com/embed/playlist/5GQIv1pWFtbYxC6T6bHz83" width="400" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
             </div>
         )
     }
