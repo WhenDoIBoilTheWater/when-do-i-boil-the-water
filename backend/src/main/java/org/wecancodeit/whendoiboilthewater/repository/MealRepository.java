cff835860392f3779ef66a41622187dec655c455
WaynePL <wayneplivingston@gmail.com>
2018-12-04 14:05:18 -0500

    removed backend folder and moving it to the when-do-i-boil-the-java repo

--- backend/src/main/java/org/wecancodeit/whendoiboilthewater/repository/MealRepository.java
+++
@@ -1,8 +0,0 @@
-package org.wecancodeit.whendoiboilthewater.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.wecancodeit.whendoiboilthewater.model.Meal;
-
-public interface MealRepository extends CrudRepository<Meal, Long> {
-
-}
