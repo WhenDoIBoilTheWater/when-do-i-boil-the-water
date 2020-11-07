02c0dac9fad2bbcf45a83519d79dd98385a66b48
Alex Greene <greene.alexanderj@gmail.com>
2020-11-07 08:49:55 -0500

    Updates

---
+++ precache-manifest.7c938226c7bd336e31b0347e2a5571a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6d745bf3b1d28d120378b090db1ca3",
+    "url": "/when-do-i-boil-the-water/index.html"
+  },
+  {
+    "revision": "a464a4d27154e321ad86",
+    "url": "/when-do-i-boil-the-water/static/css/main.6600a36c.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/when-do-i-boil-the-water/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "a464a4d27154e321ad86",
+    "url": "/when-do-i-boil-the-water/static/js/main.d19767f6.chunk.js"
+  },
+  {
+    "revision": "eebc40edb9c74217aa26",
+    "url": "/when-do-i-boil-the-water/static/js/runtime~main.f556168d.js"
+  },
+  {
+    "revision": "ff189e2175b99c62dd2115b33c36b6ca",
+    "url": "/when-do-i-boil-the-water/static/media/kitchen-timer.ff189e21.wav"
+  },
+  {
+    "revision": "4833fdfafb078206a144f9bf799e76ad",
+    "url": "/when-do-i-boil-the-water/static/media/swish-bars-03.4833fdfa.png"
+  }
+]);
